3b53f7fb1ad9fef8a9f656b1167ac1ebf74022ca
Shan Pisciotta <shanpisciotta.pro@gmail.com>
2023-10-03 20:14:00 +0200

    Création de regex pour le formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -33,7 +33,7 @@ function closeModal() {
 
 // HANDLE FORM
 // DOM Elements
-const submitBtn = document.querySelector(".btn-submit");
+let submitBtn = document.querySelector(".btn-submit");
 let firstName = document.querySelector("#first");
 let lastName = document.querySelector("#last");
 let email = document.querySelector("#email");
@@ -42,13 +42,41 @@ let quantity = document.querySelector("#quantity");
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  handleErrors();
+  handleErrors(e);
 });
 
-function handleErrors() {
-  let nameRegExp = /^[a-zA-Z-àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ\s\,\'\-]{2,}$/;
-  let emailRegExp = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
-  let birthDateRegExp = /^[0-9]{2}[0-9]{2}[0-9]{4}$/;
+function handleErrors(event) {
+  const nameRegExp = /^[a-zA-Z-àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ\s\,\'\-]{2,}$/;
+  const emailRegExp = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
+  const birthDateRegExp = /^([0-9]{4})+[-/]([0-9][0-9])+[-/]([0-9][0-9])$/;
 
-  console.log(nameRegExp.test(firstName.value));
+  // 21-01-2023 DATE
+  // 0001-01-01 DATE
+
+  let checkFirstName =  checkValues(nameRegExp, firstName, "Vous devez saisir au minimum 2 caractères.");
+  let checkLastName = checkValues(nameRegExp, lastName, "Vous devez saisir au minimum 2 caractères.");
+  let checkEmail = checkValues(emailRegExp, email, "Vous devez saisir une adresse email valide.");
+  let checkBirthDate = checkValues(birthDateRegExp, birthDate, "Vous devez saisir une date de naissance valide");
+  console.log(birthDate.value + " DATE")
+  console.log(checkBirthDate);
+
+  if(checkFirstName && checkLastName && checkEmail) {
+    console.log("hello");
+  } else {
+    event.preventDefault();
+    console.log("Une erreur dans le formulaire.");
+  }
+}
+
+function checkValues(regex, input, errorMsg) {
+  if(!regex.test(input.value)) {
+    input.parentElement.setAttribute("data-error", errorMsg);
+    input.parentElement.setAttribute("data-error-visible", "true");
+  } else {
+    input.parentElement.removeAttribute("data-error");
+    input.parentElement.removeAttribute("data-error-visible");
+  }
+
+  let isCheck = regex.test(input.value);
+  return isCheck;
 }
